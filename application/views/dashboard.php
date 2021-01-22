fc7577551285b2acfce21eb043cce930d2c4a397
Firman Permana <firman.dc50@gmail.com>
2021-01-22 18:50:59 +0700

    Templating & create dashboard admin

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,12 +1,37 @@
 <div class="container-fluid">
-    <div class="row">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo base_url('assets/img/slider1.jpg') ?>" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+    <img src="<?php echo base_url('assets/img/slider1.jpg') ?>" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+    <div class="row text-center mt-4">
         <?php foreach ($barang as $brg) :   ?>
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+            <div class="card ml-3" style="width: 16rem;">
+                <img src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title mb-2"><?php echo $brg->nama_barang ?></h5>
+                    <small><?php echo $brg->keterangan ?></small><br> 
+                    <span class="badge badge-pill badge-success mb-3">Rp. <?php echo $brg->harga ?>
+                    </span><br> 
+                    <a href="#" class="btn btn-sm btn-primary">Tambah ke keranjang</a>
+                    <a href="#" class="btn btn-sm btn-info">Detail</a>
                 </div>
             </div>
         <?php endforeach; ?>
